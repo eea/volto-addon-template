f1e7b005d7a16105b1fdd1ab4ac05f24158f98d6
Alin Voinea <contact@avoinea.com>
2022-10-24 21:49:31 +0300

    test(cypress): Upgrade to latest Volto alpha

--- .coverage.babel.config.js
+++ .coverage.babel.config.js
@@ -2,9 +2,8 @@ const defaultBabel = require('@plone/volto/babel');
 
 function applyDefault(api) {
   const voltoBabel = defaultBabel(api);
-  voltoBabel.plugins.push('@babel/plugin-transform-modules-commonjs', 'transform-class-properties', 'istanbul');
+  voltoBabel.plugins.push('istanbul');
   return voltoBabel;
 }
 
 module.exports = applyDefault;
-
