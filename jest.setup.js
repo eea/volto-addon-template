d8e954370e1bd037270e756344883b0bbac396f9
Miu Razvan <miu.razvan28@gmail.com>
2023-11-29 18:17:05 +0200

    feat: add mock for plone/components and semantic-ui-react

--- jest.setup.js
+++ jest.setup.js
@@ -10,16 +10,31 @@ global.store = mockStore({
   },
   content: {
     create: {},
+    subrequests: [],
   },
   connected_data_parameters: {},
 });
 
+const mockSemanticComponents = jest.requireActual('semantic-ui-react');
 const mockComponents = jest.requireActual('@plone/volto/components');
+
+jest.mock('semantic-ui-react', () => ({
+  ...mockSemanticComponents,
+  Popup: ({ content, trigger }) => {
+    return (
+      <div className="popup">
+        <div className="trigger">{trigger}</div>
+        <div className="content">{content}</div>
+      </div>
+    );
+  },
+}));
 
 jest.doMock('@plone/volto/components', () => {
   return {
     __esModule: true,
     ...mockComponents,
+    Toast: ({ children }) => <div className="toast">{children}</div>,
     SidebarPortal: ({ children }) => <div id="sidebar">{children}</div>,
     UniversalLink: ({ children, href }) => {
       return <a href={href}>{children}</a>;
